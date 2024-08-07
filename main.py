1b9338353f7ca85e0f57481d2f109ccf92d46606
dongaCS <109496734+dongaCS@users.noreply.github.com>
2024-08-07 14:32:42 -0400

    added 8ball function

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from dotenv import load_dotenv
 import discord
 from discord.ext import commands
 
+import random # 8ball
+
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='.', intents=intents)
@@ -16,6 +18,31 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send('pong')
 
+#.8ball works as well as .eightball
+@bot.command(aliases=['8ball']) 
+async def eightball(ctx):
+    await ctx.send(random.choice([
+        "It is certain.",
+        "It is decidedly so.",
+        "Without a doubt.",      
+        "Yes - definitely.",
+        "You may rely on it.",
+        "As I see it, yes.",
+        "Most likely.",
+        "Outlook good.",
+        "Yes.",      
+        "Signs point to yes.",
+        "Reply hazy, try again.",
+        "Ask again later.",
+        "Better not tell you now.",
+        "Cannot predict now.",
+        "Concentrate and ask again.",      
+        "Don't count on it.",
+        "My reply is no.",
+        "My sources say no.",
+        "Outlook not so good.",   
+        "Very doubtful."
+    ]))
 
 load_dotenv() # set .env variables
 bot.run(os.getenv("TOKEN"))
