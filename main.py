89c808e88aacc1e7a2b6a91d32ccadd093c3ddd5
dongaCS <109496734+dongaCS@users.noreply.github.com>
2024-08-07 19:07:18 -0400

    added load, unload and reload function for cogs

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ from dotenv import load_dotenv
 import discord
 from discord.ext import commands
 
-import random # 8ball
 
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='.', intents=intents)
 
+load_dotenv() # set .env variables
+ADMIN = [os.getenv("TEMP_ACC"), os.getenv("ALT_ACC")]
+
+
+# simple ping 
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
@@ -21,7 +25,33 @@ async def on_ready():
     print("-------------PY BOT RUNNING -------------")
 
 
-async def setup_hook():
+# load x cog
+@bot.command(hidden=True) # hides from .help
+async def load(ctx, extension):
+    if str(ctx.author.id) in ADMIN:
+        await bot.load_extension(f'cogs.{extension}')
+        await ctx.send(f'Loaded {extension}')
+
+
+# unload x cog
+@bot.command(hidden=True)
+async def unload(ctx, extension):
+    if str(ctx.author.id) in ADMIN:
+        await bot.unload_extension(f'cogs.{extension}')
+        await ctx.send(f'Unloaded {extension}')
+
+
+# reloads x cog
+@bot.command(hidden=True)
+async def reload(ctx, extension):
+    if str(ctx.author.id) in ADMIN:
+        await bot.unload_extension(f'cogs.{extension}')
+        await bot.load_extension(f'cogs.{extension}')
+        await ctx.send(f'Reloaded {extension}')
+
+
+# loads cogs on start up
+async def setup_hook(): 
     for file in os.listdir('./cogs'):
         if file.endswith('.py'):
             await bot.load_extension(f'cogs.{file[:-3]}')
@@ -30,7 +60,6 @@ async def setup_hook():
 async def main():
     async with bot:
         await setup_hook()
-        load_dotenv() # set .env variables
         await bot.start(os.getenv("TOKEN"))
 
 
