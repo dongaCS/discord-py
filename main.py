a60634a1e2cedaac55bee77dd39efba57053222b
dongaCS <109496734+dongaCS@users.noreply.github.com>
2024-08-06 21:31:07 -0400

    working bot

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("hello world")
+import os
+from dotenv import load_dotenv
+
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='.', intents=intents)
+
+@bot.event
+async def on_ready():
+    print("------------- BOT RUNNING -------------")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
+
+load_dotenv() # set .env variables
+bot.run(os.getenv("TOKEN"))
